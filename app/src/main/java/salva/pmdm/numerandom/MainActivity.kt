85dd360f61e48401e95261c32cdd8f85218d9db3
SalvadorVR <fvazquezroldan@safareyes.es>
2022-01-19 14:21:15 +0100

    Añadido thread a la lógica del número aleatorio para mostrarse después del toast

--- app/src/main/java/salva/pmdm/numerandom/MainActivity.kt
+++ app/src/main/java/salva/pmdm/numerandom/MainActivity.kt
@@ -1,6 +1,7 @@
 package salva.pmdm.numerandom
 
 import android.os.Bundle
+import android.os.SystemClock.sleep
 import android.view.Gravity
 import android.widget.Button
 import android.widget.SeekBar
@@ -21,12 +22,19 @@ class MainActivity : AppCompatActivity() {
         val toast = Toast.makeText(this@MainActivity,
             "Sorteando...", Toast.LENGTH_LONG
         )
-        toast.setGravity(Gravity.CENTER or Gravity.CENTER_HORIZONTAL, 0, 450)
+        toast.setGravity(Gravity.CENTER or Gravity.CENTER_HORIZONTAL, 0, 40)
 
         valBtnGenera.setOnClickListener {
+            valLblNumero.text = ""
             toast.show()
-            val random = (1..valSeekBar.progress).random()
-            valLblNumero.text = random.toString()
+
+            val hilo = Thread(Runnable {
+                Thread.sleep(4000);
+                val random = (1..valSeekBar.progress).random()
+                valLblNumero.text = random.toString()
+            })
+            hilo.start()
+
         }
 
     }
